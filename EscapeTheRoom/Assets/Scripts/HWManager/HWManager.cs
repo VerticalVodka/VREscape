af5c1e22583c55ce7d6a2d2f81b2f183296ba9b5
phaas <peter.haas96@gmail.com>
2018-11-30 18:47:33 +0100

    fixedErrors

--- EscapeTheRoom/Assets/Scripts/HWManager/HWManager.cs
+++ EscapeTheRoom/Assets/Scripts/HWManager/HWManager.cs
@@ -5,43 +5,46 @@ using System.Runtime.InteropServices;
 using SerialTest;
 using UnityEngine;
 
-public class HWManager : MonoBehaviour
+namespace VREscape
 {
-    private Dictionary<Enums.ButtonEnum, bool> _buttons;
-    private Dictionary<Enums.RotaryEnum, int> _rotaries;
-    private readonly InputQueue _inputQueue = new InputQueue("COM5");
-
-
-    public void Start()
+    public class HWManager : MonoBehaviour
     {
-        _buttons = new Dictionary<Enums.ButtonEnum, bool>
+        private Dictionary<Enums.ButtonEnum, bool> _buttons;
+        private Dictionary<Enums.RotaryEnum, int> _rotaries;
+        private readonly InputQueue _inputQueue = new InputQueue("COM5");
+
+
+        public void Start()
         {
-            {Enums.ButtonEnum.Button1, false},
-            {Enums.ButtonEnum.Button2, false},
-            {Enums.ButtonEnum.Button3, false},
-            {Enums.ButtonEnum.Button4, false},
-            {Enums.ButtonEnum.Rotary1, false},
-            {Enums.ButtonEnum.Rotary2, false}
-        };
-        _rotaries = new Dictionary<Enums.RotaryEnum, int>()
+            _buttons = new Dictionary<Enums.ButtonEnum, bool>
+            {
+                {Enums.ButtonEnum.Button1, false},
+                {Enums.ButtonEnum.Button2, false},
+                {Enums.ButtonEnum.Button3, false},
+                {Enums.ButtonEnum.Button4, false},
+                {Enums.ButtonEnum.Rotary1, false},
+                {Enums.ButtonEnum.Rotary2, false}
+            };
+            _rotaries = new Dictionary<Enums.RotaryEnum, int>()
+            {
+                {Enums.RotaryEnum.Rotary1, 0},
+                {Enums.RotaryEnum.Rotary2, 0}
+            };
+        }
+
+        public bool GetButtonState(Enums.ButtonEnum button)
         {
-            {Enums.RotaryEnum.Rotary1, 0},
-            {Enums.RotaryEnum.Rotary2, 0}
-        };
-    }
+            return _buttons[button];
+        }
 
-    public bool GetButtonState(Enums.ButtonEnum button)
-    {
-        return _buttons[button];
-    }
+        public int GetRotaryState(Enums.RotaryEnum rotary)
+        {
+            return _rotaries[rotary];
+        }
 
-    public int GetRotaryState(Enums.RotaryEnum rotary)
-    {
-        return _rotaries[rotary];
-    }
-
-    public void Update()
-    {
-        _inputQueue.ProcessData(out _buttons, out _rotaries);
+        public void Update()
+        {
+            _inputQueue.ProcessData(out _buttons, out _rotaries);
+        }
     }
 }

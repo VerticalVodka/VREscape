22d3176e82e23cb1179486610c34e7835d96303c
Elias Wipfler <elias.wipfler@outlook.com>
2018-11-30 19:54:57 +0100

    create rotary script

---
+++ EscapeTheRoom/Assets/Scripts/ObjectScripts/RotaryScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VREscape {
+    public class RotaryScript : MonoBehaviour {
+
+        public int StepNumber;
+        public Enums.RotaryEnum Rotary;
+        private HWManager hWManager;
+        private int oldRotaryState;
+
+        void rotate(int rotation)
+        {
+            gameObject.transform.Rotate(Vector3.forward, (360 / StepNumber * rotation));
+        }
+
+        // Use this for initialization
+        void Start() {
+            hWManager = FindObjectOfType<HWManager>();
+            oldRotaryState = hWManager.GetRotaryState(Rotary);
+        }
+
+        // Update is called once per frame
+        void Update() {
+            int newRotaryState = hWManager.GetRotaryState(Rotary);
+            if(newRotaryState != oldRotaryState)
+            {
+                rotate(newRotaryState - oldRotaryState);
+            }
+
+        }
+    }
+}

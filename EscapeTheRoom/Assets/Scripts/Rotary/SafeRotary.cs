0ec1a0e0c833830fd8f10a5b72e6424d6cb01f0e
Elias Wipfler <elias.wipfler@outlook.com>
2018-12-01 20:04:41 +0100

    fix rotary flip mechanic, change safe riddle for CW/CCW workflow (not yet done)

--- EscapeTheRoom/Assets/Scripts/Rotary/SafeRotary.cs
+++ EscapeTheRoom/Assets/Scripts/Rotary/SafeRotary.cs
@@ -3,5 +3,25 @@ namespace VREscape
 {
     public class SafeRotary : Rotary
     {
+        public int LastState;
+
+        public override void Start()
+        {
+            flips = true;
+            base.Start();
+            LastState = StartValue;
+        }
+
+
+        public override void Update()
+        {
+            LastState = CurrentState;
+            base.Update();
+        }
+
+        public bool HasTurned()
+        {
+            return LastState != CurrentState;
+        }
     }
 }

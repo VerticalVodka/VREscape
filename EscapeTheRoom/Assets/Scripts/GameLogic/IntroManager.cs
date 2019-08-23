5f7e5e66fd8416111c62d800e31ffd181896c641
SourceOverflow <oscarablinger@gmail.com>
2019-08-23 18:31:49 +0200

    Fix Intro -> SampleScene transition error

---
+++ EscapeTheRoom/Assets/Scripts/GameLogic/IntroManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VREscape
+{
+    public class IntroManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.L))
+            {
+                StartGame();
+            }
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
